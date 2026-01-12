d1b33c5adfc1a78a4c98f6d144930a8b0e728387
qingjiu9165 <15550136+qingjiu9165@user.noreply.gitee.com>
2026-01-12 16:47:27 +0800

    Reno?checksum

--- TCP_Test_1/src/com/ouc/tcp/test/CheckSum.java
+++ TCP_Test_1/src/com/ouc/tcp/test/CheckSum.java
@@ -10,7 +10,6 @@ public class CheckSum {
 	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
 	public static short computeChkSum(TCP_PACKET tcpPack) {
 		CRC32 crc32 = new CRC32();
-
 		int seq = tcpPack.getTcpH().getTh_seq();
 		int ack = tcpPack.getTcpH().getTh_ack();
 
@@ -29,12 +28,10 @@ public class CheckSum {
 				crc32.update((dataInt >>> 24) & 0xFF);
 				crc32.update((dataInt >>> 16) & 0xFF);
 				crc32.update((dataInt >>> 8) & 0xFF);
-				crc32.update(ack & 0xFF);
+				crc32.update((dataInt) & 0xFF);
 			}
 		}
-
 		long crcValue = crc32.getValue();
-
 		short checkSum = (short) ((crcValue ^ (crcValue >>> 16)) & 0xFFFF);
 
 		return (short) checkSum;
